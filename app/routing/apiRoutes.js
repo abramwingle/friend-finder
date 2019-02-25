66e65f17e24218e4d04493e744000b49cb31a849
Abram Wingle <abramwingle15@gmail.com>
2019-02-24 21:00:17 -0700

    mostly done just ned to fix a few buggies

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,37 @@ module.exports = function (app) {
 
     app.post("/api/friends"), function (req, res) {
 
+        var user = req.body;
+
+        for (i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+
+        }
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+
+        for (i = 0; i < friends.length; i++) {
+            var difference = 0;
+
+            for (var f = 0; f < friends[i].scores.length; f++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+
+
+            }
+
+            if(totalDifference < minimumDifference) {
+                bestFriendIndex = i;
+                minimumDifference = totalDifference;
+              }
+            }
+        friends.push (user)
+        res.json(friends[bestFriendIndex]);
+
+        }
+
     }
 
 
-}
+
 
