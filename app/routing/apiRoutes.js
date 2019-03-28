d8bb70478e1159c04ed3d2d4c2f058dd7a527da0
Abram Wingle <abramwingle15@gmail.com>
2019-03-28 02:12:48 -0600

    only need to get modal working and there is a random error with the parse int

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,51 @@
-var express = require('express')
-var app = express()
+
+var friends = require("../data/friends");
 
 
 module.exports = function (app) {
+
     app.get("/api/friends", function (req, res) {
+        console.log("entered the get api/friends")
+
         res.json(friends);
+
     });
 
-    app.post("/api/friends"), function (req, res) {
-
+    app.post("/api/friends", function (req, res) {
+        console.log("entered post api/friends");
+        console.log(req.body['scores[]']);
         var user = req.body;
 
-        for (i = 0; i < user.scores.length; i++) {
-            user.scores[i] = parseInt(user.scores[i]);
+        user.scores = [];
+
+
+
+        for (friendIterator = 0; friendIterator < req.body["scores[]"].length; friendIterator++) {
+            user.scores.push(parseInt(req.body["scores[]"][friendIterator]));
 
         }
         var bestFriendIndex = 0;
         var minimumDifference = 40;
 
-        for (i = 0; i < friends.length; i++) {
+        for (friendIterator = 0; friendIterator < friends.length; friendIterator++) {
             var difference = 0;
-
-            for (var f = 0; f < friends[i].scores.length; f++) {
-                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+            var totalDifference;
+            for (var friendScoreIterator = 0; friendScoreIterator < friends[friendIterator].scores.length; friendScoreIterator++) {
+                var difference = Math.abs(user.scores[friendScoreIterator] - friends[friendScoreIterator]);
                 totalDifference += difference;
-
-
             }
 
-            if(totalDifference < minimumDifference) {
-                bestFriendIndex = i;
+            if (totalDifference < minimumDifference) {
+                bestFriendIndex = friendIterator;
                 minimumDifference = totalDifference;
-              }
             }
-        friends.push (user)
-        res.json(friends[bestFriendIndex]);
-
         }
+        friends.push(user)
+        res.json(friends[bestFriendIndex]);
+        console.log("finished post api/friends");
+    });
 
-    }
+}
 
 
 
