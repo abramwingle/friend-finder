6a45087df8d335667520e6fe879e8203fd061e21
Abram Wingle <abramwingle15@gmail.com>
2019-04-16 19:55:44 -0600

    everything is working just need the finishing details

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,37 +2,37 @@ var friends = [
     {
         name: "Ross",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [1,2,5,4,1,2,3,4,2,1,2]
     },
     {
         name: "Chandler",
         photo: "https://vignette.wikia.nocookie.net/friends/images/c/cc/Square_Chandler.jpg/revision/latest/scale-to-width-down/200?cb=20111216200026",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [5,4,3,2,1,5,4,3,2,1,4]
     },
     {
         name: "Rachel",
         photo: "https://vignette.wikia.nocookie.net/friends/images/e/e3/Rachel-Season-5.png/revision/latest/scale-to-width-down/176?cb=20180127172230",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [1,2,3,4,5,5,4,3,2,1,5]
     },
     {
         name: "Joey",
         photo: "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest?cb=20180424154245",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [5,2,1,4,5,5,1,4,5,1,1]
     },
     {
         name: "Pheobe",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/220px-Friendsphoebe.jpg",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [1,1,1,1,1,1,1,1,1,1,3]
     },
     {
         name: "Monica",
         photo: "https://imgix.bustle.com/rehost/2016/9/13/04a388e1-335b-4fad-969f-383866bddab7.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [2,2,2,2,2,2,2,2,2,2,4], 
     },
     {
         name: "Ben",
         photo: "https://vignette.wikia.nocookie.net/friends/images/8/8e/Ben.png/revision/latest?cb=20180324151610",
-        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+        scores: [5,5,5,5,5,5,5,5,5,2,2]
     },
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,33 +13,36 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         console.log("entered post api/friends");
-        console.log(req.body['scores[]']);
         var user = req.body;
-
         user.scores = [];
 
 
 
-        for (friendIterator = 0; friendIterator < req.body["scores[]"].length; friendIterator++) {
-            user.scores.push(parseInt(req.body["scores[]"][friendIterator]));
+        for (var i = 0; i < req.body['scores[]'].length; i++) {
 
+            var parsedArray = parseInt(req.body["scores[]"][i]);
+            user.scores.push(parsedArray);
         }
-        var bestFriendIndex = 0;
-        var minimumDifference = 40;
+        var bestFriendIndex;
+        var minimumDifference = 20;
 
         for (friendIterator = 0; friendIterator < friends.length; friendIterator++) {
-            var difference = 0;
-            var totalDifference;
+
+            var totalDifference = 0;
             for (var friendScoreIterator = 0; friendScoreIterator < friends[friendIterator].scores.length; friendScoreIterator++) {
-                var difference = Math.abs(user.scores[friendScoreIterator] - friends[friendScoreIterator]);
+
+                var difference = Math.abs(user.scores[friendScoreIterator] - friends[friendIterator].scores[friendScoreIterator]);
                 totalDifference += difference;
             }
 
             if (totalDifference < minimumDifference) {
                 bestFriendIndex = friendIterator;
                 minimumDifference = totalDifference;
+                console.log(bestFriendIndex);
+                //console.log(friends[bestFriendIndex]);
             }
         }
+
         friends.push(user)
         res.json(friends[bestFriendIndex]);
         console.log("finished post api/friends");
